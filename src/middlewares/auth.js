0a8eedd5eb1ce7490726cf0416e1bbe082fb8f38
ubuntu <shoyatbekov03032003@gmail.com>
2024-03-11 00:01:30 +0500

    .env updated

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,7 +2,6 @@ const admin = require("../utils/firebase/index.js")
 
 const authMiddleware = async (req, res, next) => {
     const token = req.headers.authorization;
-    console.log("Token ", token);
     if (!token) return res.status(401).json({ error: 'Unauthorized' });
 
     try {
--- src/utils/firebase/index.js
+++ src/utils/firebase/index.js
@@ -3,10 +3,9 @@
 const firebaseAdmin = require("firebase-admin");
 const configs = require("../../configs/index.js");
 
-const serverKey = require("../../../firebase-credentials.json");
 firebaseAdmin.initializeApp({
-    credential: firebaseAdmin.credential.cert(serverKey),
-    databaseURL: "https://quizy-fd701-default-rtdb.firebaseio.com"
+    credential: firebaseAdmin.credential.cert(configs.firebase),
+    databaseURL: configs.firebase.databaseURL
 })
 
 module.exports = firebaseAdmin;
